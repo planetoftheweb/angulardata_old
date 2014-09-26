b110e948d90717fb1897f3214ee0bd3b3ee7e4b2
Ray Villalobos <ray@lynda.com>
2014-09-26 02:30:28 -0400

    ch05_02 Registration Factory

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -2,8 +2,8 @@ myApp.controller('RegistrationController',
   function($scope, $firebaseSimpleLogin, $location, Authentication) {
   
   $scope.login = function() {
-      Authentication.login($scope.user);
-    }).then(function(user) {
+    Authentication.login($scope.user)
+      .then(function(user) {
       $location.path('/meetings');
     }, function(error) {
       $scope.message = error.toString();
