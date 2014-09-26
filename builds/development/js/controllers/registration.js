d3b72addb8a7ca14a9bd8ca3102619905511db81
Ray Villalobos <ray@lynda.com>
2014-09-26 01:47:54 -0400

    ch05_01 Adding Simple Login

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,8 +1,18 @@
 myApp.controller('RegistrationController', 
-  function($scope, $location) {
+  function($scope, $firebaseSimpleLogin, $location) {
+
+  var ref = new Firebase('https://attendanceldcapp.firebaseio.com/');
+  var simpleLogin = $firebaseSimpleLogin(ref);
   
   $scope.login = function() {
-    $location.path('/meetings');
+    simpleLogin.$login('password', {
+      email: $scope.user.email,
+      password: $scope.user.password
+    }).then(function(user) {
+      $location.path('/meetings');
+    }, function(error) {
+      $scope.message = error.toString();
+    });
   } //login
 
   $scope.register = function() {
