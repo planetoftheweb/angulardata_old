cd9006e6c1973cae10ff40a699652d752c972ba7
Ray Villalobos <ray@lynda.com>
2014-09-27 01:33:04 -0400

    testing userinfo

--- builds/development/js/controllers/status.js
+++ builds/development/js/controllers/status.js
@@ -1,6 +1,6 @@
 myApp.controller('StatusController', function(
   $scope, $rootScope, $firebaseSimpleLogin,
-    $location, Authentication) {
+    $location, Authentication, $firebase, FIREBASE_URL) {
 
   $scope.logout = function() {
     Authentication.logout();
@@ -8,11 +8,16 @@ myApp.controller('StatusController', function(
   } //logout
 
   $rootScope.$on('$firebaseSimpleLogin:login', function(e, authUser) {
-    $scope.userEmail = authUser.email;
+    var ref = new Firebase(FIREBASE_URL + '/users/' + authUser.uid);
+    var user = $firebase(ref).$asObject();
+
+    user.$loaded().then(function() {
+      $rootScope.currentUser = user;
+    });
   }); //$firebaseSimpleLogin:login
 
   $rootScope.$on('$firebaseSimpleLogin:logout', function(e, authUser) {
-    $scope.userEmail = null;
+    $rootScope.currentUser = null;
   }); //$firebaseSimpleLogin:login
 
 }); //StatusController
