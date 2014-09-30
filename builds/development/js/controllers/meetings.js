4d6976d808933ca8d95ced65890af2efd27b996e
Ray Villalobos <ray@lynda.com>
2014-09-30 01:27:52 -0400

    fixed uid login issue

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -7,37 +7,39 @@ var simpleLogin = $firebaseSimpleLogin(ref);
 
   simpleLogin.$getCurrentUser().then(function(authUser) {
 
-    var ref = new Firebase(FIREBASE_URL + '/users/' 
-        + authUser.uid + '/meetings');
-    var meetingsInfo = $firebase(ref);
-    var meetingsObj = $firebase(ref).$asObject();
-    var meetingsArray = $firebase(ref).$asArray();
+    if (authUser !== null) {
+      var ref = new Firebase(FIREBASE_URL + '/users/' 
+          + authUser.uid + '/meetings');
+      var meetingsInfo = $firebase(ref);
+      var meetingsObj = $firebase(ref).$asObject();
+      var meetingsArray = $firebase(ref).$asArray();
 
-    meetingsObj.$loaded().then(function(data) {
-      $scope.meetings = meetingsObj;
-    }); // meetings Object Loaded
+      meetingsObj.$loaded().then(function(data) {
+        $scope.meetings = meetingsObj;
+      }); // meetings Object Loaded
 
-    meetingsArray.$loaded().then(function(data) {
-      $rootScope.howManyMeetings = meetingsArray.length;
-    }); // meetings Array Loaded
+      meetingsArray.$loaded().then(function(data) {
+        $rootScope.howManyMeetings = meetingsArray.length;
+      }); // meetings Array Loaded
 
-    meetingsArray.$watch(function(event) {
-      $rootScope.howManyMeetings = meetingsArray.length;
-    });
-
-    $scope.addMeeting=function() {
-      meetingsInfo.$push({
-        name: $scope.meetingname,
-        date: Firebase.ServerValue.TIMESTAMP
-      }).then(function() {
-        $scope.meetingname = '';
+      meetingsArray.$watch(function(event) {
+        $rootScope.howManyMeetings = meetingsArray.length;
       });
-    } //addmeeting
 
-    $scope.deleteMeeting=function(key) {
-      meetingsInfo.$remove(key);
-    } //deletemeeting
+      $scope.addMeeting=function() {
+        meetingsInfo.$push({
+          name: $scope.meetingname,
+          date: Firebase.ServerValue.TIMESTAMP
+        }).then(function() {
+          $scope.meetingname = '';
+        });
+      } //addmeeting
 
+      $scope.deleteMeeting=function(key) {
+        meetingsInfo.$remove(key);
+      } //deletemeeting
+
+    } // user exists
 
   }); //get current user
 
