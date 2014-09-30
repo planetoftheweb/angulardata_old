84212a6489525684d9737d1aeeff1e5a0cddbc99
Ray Villalobos <ray@lynda.com>
2014-09-30 11:39:58 -0400

    ch06_04 added checkins

--- builds/development/js/controllers/checkins.js
+++ builds/development/js/controllers/checkins.js
@@ -1,6 +1,25 @@
-myApp.controller('CheckInsController', 
-  function($scope, $rootScope, 
-    $firebase, $firebaseSimpleLogin, FIREBASE_URL) {
+myApp.controller('CheckInsController', function(
+  $scope, $rootScope, $firebase, $routeParams,
+  $firebaseSimpleLogin, FIREBASE_URL) {
 
+  $scope.whichmeeting = $routeParams.mId;
+  $scope.whichuser = $routeParams.uId;
+
+  var ref = new Firebase(FIREBASE_URL + '/users/' + $scope.whichuser + '/meetings/' + $scope.whichmeeting + '/checkins');
+
+  $scope.addCheckin = function() {
+    var checkinsObj = $firebase(ref);
+
+    var myData = {
+      firstname: $scope.user.firstname,
+      lastname: $scope.user.lastname,
+      email: $scope.user.email,
+      date: Firebase.ServerValue.TIMESTAMP
+    };
+
+    checkinsObj.$push(myData).then(function() {
+      //send to our list of checkins page
+    }); //data sent to firebase.
+  } //addCheckin
 
 }); //CheckInsController
