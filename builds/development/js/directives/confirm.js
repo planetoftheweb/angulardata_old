9409dd20dd962658f5d5db70902b4e73dcc24def
Ray Villalobos <ray@lynda.com>
2014-10-01 19:37:07 -0400

    Directives issue

---
+++ builds/development/js/directives/confirm.js
@@ -0,0 +1,15 @@
+myApp.directive('confirmationNeeded', function() {
+  return {
+    priority: 1,
+    terminal: true,
+    link: function(scope, element, attr) {
+      var msg = attr.confirmationNeeded || "Are you sure you want to delete?";
+      var clickAction = attr.ngClick;
+      element.bind('click', function() {
+        if (window.confirm(msg) {
+          scope.$eval(clickAction);
+        })// confirm
+      }); //click
+    } // link
+  } //return
+}); //confirmationNeeded
